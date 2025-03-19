92d1e42ddf28134f340b638eaacb32f50b094af6
Mohammad karshnas <mkarshnas6@gmail.com>
2025-03-19 16:15:12 +0330

    add partition to api and show

--- app/src/main/java/com/mkarshnas6/karenstudio/karengold/MainActivity.kt
+++ app/src/main/java/com/mkarshnas6/karenstudio/karengold/MainActivity.kt
@@ -2,11 +2,8 @@ package com.mkarshnas6.karenstudio.karengold
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mkarshnas6.karenstudio.karengold.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -18,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: PriceAdapter
     private val priceList = mutableListOf<PriceItem>()
+    private var allGoldPrices: List<PriceItem> = emptyList()
+    private var allCurrencyPrices: List<PriceItem> = emptyList()
+    private var allCryptoPrices: List<PriceItem> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,15 +31,45 @@ class MainActivity : AppCompatActivity() {
         binding.recylerPrice.adapter = adapter
         binding.recylerPrice.layoutManager = LinearLayoutManager(this)
 
+        // Fetch the data
         fetchData()
+
+        // Set OnClickListeners for filtering
+        binding.txtPriceGold.setOnClickListener {
+            showGoldPrices()
+            binding.txtPriceGold.setTextColor(getColor(R.color.gold_text))
+            binding.txtPriceCrypto.setTextColor(getColor(R.color.white_text))
+            binding.txtPriceMoney.setTextColor(getColor(R.color.white_text))
+
+        }
+
+        binding.txtPriceCrypto.setOnClickListener {
+            showCryptoPrices()
+            binding.txtPriceGold.setTextColor(getColor(R.color.white_text))
+            binding.txtPriceCrypto.setTextColor(getColor(R.color.gold_text))
+            binding.txtPriceMoney.setTextColor(getColor(R.color.white_text))
+        }
+
+        binding.txtPriceMoney.setOnClickListener {
+            showCurrencyPrices()
+            binding.txtPriceGold.setTextColor(getColor(R.color.white_text))
+            binding.txtPriceCrypto.setTextColor(getColor(R.color.white_text))
+            binding.txtPriceMoney.setTextColor(getColor(R.color.gold_text))
+        }
     }
 
     private fun fetchData() {
         RetrofitClient.instance.getPrices().enqueue(object : Callback<PriceResponse> {
             override fun onResponse(call: Call<PriceResponse>, response: Response<PriceResponse>) {
                 if (response.isSuccessful && response.body() != null) {
+                    // Save the data separately for gold, currency, and cryptocurrency
+                    allGoldPrices = response.body()!!.gold
+                    allCurrencyPrices = response.body()!!.currency
+                    allCryptoPrices = response.body()!!.cryptocurrency
+
+                    // Initially show all prices (or can be changed to only show gold)
                     priceList.clear()
-                    priceList.addAll(response.body()!!.gold + response.body()!!.currency + response.body()!!.cryptocurrency)
+                    priceList.addAll(allGoldPrices)
                     adapter.notifyDataSetChanged()
                 }
             }
@@ -49,5 +79,26 @@ class MainActivity : AppCompatActivity() {
                     .show()
             }
         })
+    }
+
+    // Function to display only Gold prices
+    private fun showGoldPrices() {
+        priceList.clear()
+        priceList.addAll(allGoldPrices)
+        adapter.notifyDataSetChanged()
+    }
+
+    // Function to display only Currency prices
+    private fun showCurrencyPrices() {
+        priceList.clear()
+        priceList.addAll(allCurrencyPrices)
+        adapter.notifyDataSetChanged()
+    }
+
+    // Function to display only Crypto prices
+    private fun showCryptoPrices() {
+        priceList.clear()
+        priceList.addAll(allCryptoPrices)
+        adapter.notifyDataSetChanged()
     }
 }
