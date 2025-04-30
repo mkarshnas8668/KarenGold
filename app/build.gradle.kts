0bd0b1706d825662b79229f4b0cf751e9c09e487
Mohammad karshnas <mkarshnas6@gmail.com>
2025-04-30 12:29:45 +0330

    set version to 1.1 and versioncode to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.mkarshnas6.karenstudio.karengold"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
